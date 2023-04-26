6ca657110c89e2736ab39fbcce19d68739ffc231
Yer Yang <yyhmg_86@yahoo.com>
2023-04-25 21:20:49 -0700

    Completes Array Filter Method Lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+function findMatching(array, attribute) {
+    return array.filter(function (array) {
+        return array.toLowerCase() === attribute.toLowerCase()
+    })
+}
+console.log(findMatching(drivers, "bobby"))
+function fuzzyMatch(array, attribute) {
+    return array.filter(function (array) {
+        return array.toLowerCase().charAt(0) === attribute.toLowerCase().charAt(0)
+    })
+}
+console.log(fuzzyMatch(drivers, "Brianna"))
+function matchName(array, arg) {
+    return array.filter(function (array) {
+        return array.name === arg
+    })
+}
+ console.log(matchName(drivers, "Bobby"))
